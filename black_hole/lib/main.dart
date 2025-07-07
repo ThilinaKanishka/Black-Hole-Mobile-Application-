cba56201f7ae320ae3c032a36a9299ef7430b75f
ThilinaKanishka <thilina20010313@gmail.com>
2025-07-07 18:45:24 +0530

    Update main.dart

--- black_hole/lib/main.dart
+++ black_hole/lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
           ],
         ),
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16),
           child: Column(
             children: [
               Text(
