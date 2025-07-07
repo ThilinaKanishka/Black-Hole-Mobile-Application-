5a49bafdd7812f62b6c3c4c24c917403fbb932a1
ThilinaKanishka <thilina20010313@gmail.com>
2025-07-07 15:29:51 +0530

    Update main.dart

--- black_hole/lib/main.dart
+++ black_hole/lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "sapce App", home: Scaffold());
+    return MaterialApp(
+      title: "sapce App",
+      home: Scaffold(backgroundColor: Colors.black),
+    );
   }
 }
