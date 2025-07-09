d16d00ccf6fc026e3407eeed5f85f0b18860846f
ThilinaKanishka <thilina20010313@gmail.com>
2025-07-09 14:19:30 +0530

    Update main.dart

--- black_hole/lib/main.dart
+++ black_hole/lib/main.dart
@@ -297,6 +297,10 @@ class _SpaceHomePageState extends State<SpaceHomePage>
                 ),
               ),
               const SizedBox(height: 10),
+              Text(
+                "Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed  .",
+                style: TextStyle(color: Colors.white, fontSize: 10),
+              ),
             ],
           ),
         ),
